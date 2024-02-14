94e8db945bf59167421a1d87abe54f62dee91448
Edson Carlos de Melo Ferreira <edsoncarlosdemeloferreira@gmail.com>
2024-02-14 19:23:53 -0300

    Correção da class Batalha.Trabalhando com varios layouts

--- EFCore.WebAPI/EFCore.WebAPI/Models/Batalha.cs
+++ EFCore.WebAPI/EFCore.WebAPI/Models/Batalha.cs
@@ -7,7 +7,7 @@ namespace EFCore.WebAPI.Models
 {
     public class Batalha
     {
-        public int Id { get; set; }¹¹¹
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public DateTime DtInicio { get; set; }
